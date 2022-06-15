5895559a95a70282fef3938e78924646b743c9eb
LauraElenaAnghelache <laura.elena.anghelache@outlook.it>
2022-06-15 13:54:47 +0200

    final commit

--- src/jana60/shop/Main.java
+++ src/jana60/shop/Main.java
@@ -3,8 +3,37 @@ package jana60.shop;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		/*Stiamo lavorando per un sito di e-commerce e dobbiamo gestirne i prodotti.
+		Consegna:
+		Nel progetto java-oop-shop, package jana60.shop e creare la classe Prodotto che gestisce i prodotti dello shop.
+		Un prodotto è caratterizzato da:
+		marca
+		nome
+		prezzo
+		iva
+		Scegliere opportunamente il tipo di dato per gli attributi
+		Aggiungere un costruttore che inizializza tutti gli attributi.
+		Aggiungere dei metodi per:
+		calcolare il prezzo comprensivo di iva
+		formattare il prezzo restituendo una stringa con un determinato numero di decimali e il carattere €
+		Per testare le funzionalità della classe prodotto aggiungere una classe Main con metodo main dove istanziare degli oggetti Prodotto e chiamarne i vari metodi (i valori degli attributi possono essere scelti arbitrariamente da voi programmatori)
+		*/
+		Prodotto[] Prodotti = new Prodotto[4];
+		Prodotto smartphone = new Prodotto("Samsung", "Galaxy", 350f, 22);
+		Prodotto cuffieWireless = new Prodotto("Apple", "Airpods",170f, 22);
+		Prodotto tv = new Prodotto("Lg", "Smart Tv", 400f, 22);
+		Prodotto tablet = new Prodotto("Lenovo", "IdeaPad", 199f, 22);
+		
+		Prodotti[0] = smartphone;
+		Prodotti[1] = cuffieWireless;
+	    Prodotti[2] = tv;
+	    Prodotti[3] = tablet;
+	    
+	    for(int i=0; i<Prodotti.length; i++) {
+	    	System.out.println(Prodotti[i].toString());
+	    	
+	    	}
+	    }
 	}
 
-}
+
--- src/jana60/shop/Prodotto.java
+++ src/jana60/shop/Prodotto.java
@@ -1,10 +1,44 @@
 package jana60.shop;
 
+import java.text.DecimalFormat;
+
 public class Prodotto {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		/*Stiamo lavorando per un sito di e-commerce e dobbiamo gestirne i prodotti.
+Consegna:
+Nel progetto java-oop-shop, package jana60.shop e creare la classe Prodotto che gestisce i prodotti dello shop.
+Un prodotto è caratterizzato da:
+marca
+nome
+prezzo
+iva
+Scegliere opportunamente il tipo di dato per gli attributi
+Aggiungere un costruttore che inizializza tutti gli attributi.
+Aggiungere dei metodi per:
+calcolare il prezzo comprensivo di iva
+formattare il prezzo restituendo una stringa con un determinato numero di decimali e il carattere €
+Per testare le funzionalità della classe prodotto aggiungere una classe Main con metodo main dove istanziare degli oggetti Prodotto e chiamarne i vari metodi (i valori degli attributi possono essere scelti arbitrariamente da voi programmatori)
+*/
+	String marca, nome;
+	float prezzoBase;
+	int iva;
+	DecimalFormat df = new DecimalFormat ("#.00€");
+     Prodotto(String marca, String nome, float prezzoBase, int iva ) {
+		super();
+		this.marca = marca;
+		this.nome = nome;
+		this.prezzoBase = prezzoBase;
+		this.iva = iva;
+	}
+	float prezzoTotale()
+	{
+		return prezzoBase + ((prezzoBase/100)*iva);
+	}
+	public String toString()
+	{
+		return "Il prezzo del prodotto" +nome+marca+" è: " + df.format(prezzoTotale());
+	}
 	}
 
-}
+
+
